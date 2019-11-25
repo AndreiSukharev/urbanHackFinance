228960ebeffdadc917c3b8427d7224a664859911
vladisslaww <vladisslaww@gmail.com>
2019-11-25 17:47:06 +0300

    Added home page placeHolder

---
+++ client/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-container fluid>
+        <v-row no-gutters justify="center">
+            <h1>Home</h1>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+
+</style>
